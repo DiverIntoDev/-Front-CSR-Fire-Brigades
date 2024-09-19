c849e246b2636ebbcd59f5117e4052fee51720a4
AUGUSTO MITTMANN <augusto.mittmann@sap.com>
2024-09-18 21:29:13 -0300

    installing supabase and testing

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,23 @@
-export default function Home() {
+import supabase from "./config/supabaseClient";
+
+async function Home() {
+
+  let { data: brigades, error } = await supabase
+  .from('brigades')
+  .select('');
+
+  if (error) {
+    console.error('Erro ao buscar dados:', error);
+  } else {
+    console.log('Dados encontrados:', brigades);
+    
+  }
+
   return (
     <div>
-      a
+      Conexão Brigadas
     </div>
   );
 }
+
+export default Home
