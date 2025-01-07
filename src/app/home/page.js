0aab01ac6341880b7a86f029fba5dd8f9cab36b0
AUGUSTO MITTMANN <augusto.mittmann@sap.com>
2025-01-06 22:23:10 -0300

    fix error

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -20,18 +20,18 @@ import { Fragment } from "react";
 
 function Home() {
     return (
-      <div className="wrapper">
+      <><div className="wrapper">
         <div>
           Página atual: Inicial
         </div>
         <div>
           <Link href="/contact">
-              Contato
+            Contato
           </Link>
         </div>
         <div>
           <Link href="/brigades">
-              Brigadas
+            Brigadas
           </Link>
         </div>
         <header className="header">
@@ -39,11 +39,10 @@ function Home() {
             <MenuButton />
           </div>
           <div className="menuCenter">
-          <Image
+            <Image
               src={Icons.logo.value}
               alt={Icons.logo.alt}
-              width={160}
-              />
+              width={160} />
           </div>
           <div className="menuRight">
             <HelpButton />
@@ -91,66 +90,64 @@ function Home() {
             <p>Pergunta 3</p>
           </div>
         </footer>
-      </div>
-      <div className="wrapper">
-        <header className="header">
-          <div className="menuLeft">
-            <MenuButton />
-          </div>
-          <div className="menuCenter">
-          <Image
-              src={Icons.logo.value}
-              alt={Icons.logo.alt}
-              width={160}
-              />
-          </div>
-          <div className="menuRight">
-            <HelpButton />
-          </div>
-        </header>
-        <section className="section">
-          <div className="brigadasTitle">
-            Brigadas
-          </div>
-          <div>
-            Encontre as brigadas mais próximas de você ou cadastre a sua brigada.
-          </div>
-          <div className="viewBrigadesButton">
-            <ViewBrigades />
-          </div>
-          <div>
-            <AddNewBrigade />
-          </div>
-          <div className="feedTitle">
-            Feed de Notícias
-          </div>
-          <div>
-            Encontre aqui as publicações de nossas brigadas voluntárias nas redes sociais.
-          </div>
-          <div className="filterSide">
-            <div className="searchBar">
-              <Input placeholder="Pesquisar notícia" />
+      </div><div className="wrapper">
+          <header className="header">
+            <div className="menuLeft">
+              <MenuButton />
             </div>
-            <div className="buttonSearch">
-              <FilterButton />
+            <div className="menuCenter">
+              <Image
+                src={Icons.logo.value}
+                alt={Icons.logo.alt}
+                width={160} />
             </div>
-          </div>
-        </section>
-        <footer className="footer">
-          <div className="footerColumn">
-            <p>Entre em contato</p>
-            <p>Contato 1</p>
-            <p>Contato 2</p>
-            <p>Contato 3</p>
-          </div>
-          <div className="footerColumn">
-            <p>FAQ</p>
-            <p>Pergunta 1</p>
-            <p>Pergunta 2</p>
-            <p>Pergunta 3</p>
-          </div>
-        </footer>
-      </div>
+            <div className="menuRight">
+              <HelpButton />
+            </div>
+          </header>
+          <section className="section">
+            <div className="brigadasTitle">
+              Brigadas
+            </div>
+            <div>
+              Encontre as brigadas mais próximas de você ou cadastre a sua brigada.
+            </div>
+            <div className="viewBrigadesButton">
+              <ViewBrigades />
+            </div>
+            <div>
+              <AddNewBrigade />
+            </div>
+            <div className="feedTitle">
+              Feed de Notícias
+            </div>
+            <div>
+              Encontre aqui as publicações de nossas brigadas voluntárias nas redes sociais.
+            </div>
+            <div className="filterSide">
+              <div className="searchBar">
+                <Input placeholder="Pesquisar notícia" />
+              </div>
+              <div className="buttonSearch">
+                <FilterButton />
+              </div>
+            </div>
+          </section>
+          <footer className="footer">
+            <div className="footerColumn">
+              <p>Entre em contato</p>
+              <p>Contato 1</p>
+              <p>Contato 2</p>
+              <p>Contato 3</p>
+            </div>
+            <div className="footerColumn">
+              <p>FAQ</p>
+              <p>Pergunta 1</p>
+              <p>Pergunta 2</p>
+              <p>Pergunta 3</p>
+            </div>
+          </footer>
+        </div></>
     );
   }
 
