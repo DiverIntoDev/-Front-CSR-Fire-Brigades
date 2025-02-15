a288d8e5dfcd2a32dfc32b20efea826bc5c21bf3
Felipe Flores <felipe.flores02@sap.com>
2025-02-15 19:19:07 -0300

    Fix phone formatter

--- src/app/formatters/phoneFormatter.js
+++ src/app/formatters/phoneFormatter.js
@@ -6,6 +6,10 @@ export default class PhoneFormatter {
     if (phone.length === 9) {
       return `${phone}-`;
     }
-    return phone.replace(/(\d{2})(\d{5})(\d{4})/, "($1) $2-$3");
+    if (phone.length === 15) {
+      // replace (12) 1234-12345 to (12) 12345-1234
+      return phone.replace(/(\(\d{2}\))\s(\d{4})-(\d{5})/, "$1 $3-$2");
+    }
+    return phone.replace(/(\d{2})(\d{4})-(\d{5})/, "($1) $2-$3");
   }
 }
