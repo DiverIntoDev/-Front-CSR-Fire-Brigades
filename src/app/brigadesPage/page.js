8d5aac2730b8026b6bea91c70aa70b21d481310f
Felipe Flores <felipe.flores02@sap.com>
2025-05-08 20:13:09 -0300

    Add initial page for brigades detail

--- src/app/brigadesPage/page.js
+++ src/app/brigadesPage/page.js
@@ -1,24 +1,37 @@
-import Link from "next/link";
+'use client'
+
+import { useState } from "react";
+import Header from "../components/header";
+import Image from "next/image";
+import Footer from "../components/footer";
+import Icons from "../constants/icons";
 
 function Brigades() {
+  const [brigadeName, setBrigadeName] = useState("Nome da Brigada");
+  const [location, setLocation] = useState("Rua da Brigada, 123 - Bairro Centro, Cidade - Estado");
 
-    return (
-      <>
-        <div>
-          Página atual:  Brigadas
+  return (
+    <div style={{opacity: "1", transition: "opacity 0.5s"}}>
+      <Header/>
+      <div>
+        <span style={{color: "#39542D", fontWeight: "bolder", width: "100%", marginBottom: "1rem", font: "bold bold bold 20px/24px 'Montserrat'", fontFamily: "'Montserrat', sans-serif"}}>Perfil da Brigada</span>
+        <div style={{display: "flex"}}>
+          <Image src={Icons.adicionar.value} alt="Brigade" width={100} height={100} style={{borderRadius: "50%", marginBottom: "1rem"}}/>
+          <div>
+            <div style={{display: "flex", flexWrap: "wrap"}}>
+              <span style={{color: "#DDA15E", fontWeight: "bolder", width: "100%", font: "normal normal bold 18px/22px 'Montserrat'", fontFamily: "'Montserrat', sans-serif"}}>Nome:</span>
+              <span style={{color: "#39542D", width: "100%", font: "normal normal normal 14px/18px 'Montserrat'", fontFamily: "'Montserrat', sans-serif"}}>{brigadeName}</span>
+            </div>
+            <div style={{display: "flex", flexWrap: "wrap"}}>
+              <span style={{color: "#DDA15E", fontWeight: "bolder", width: "100%", font: "normal normal bold 18px/22px 'Montserrat'", fontFamily: "'Montserrat', sans-serif"}}>Localização:</span>
+              <span style={{color: "#39542D", width: "100%", font: "normal normal normal 14px/18px 'Montserrat'", fontFamily: "'Montserrat', sans-serif"}}>{location}</span>
+            </div>
+          </div>
         </div>
-        <div>
-          <Link href="/contactPage">
-              Contato
-          </Link>
-        </div>
-        <div>
-          <Link href="/home">
-              Home
-          </Link>
-        </div>
-      </>
+      </div>
+      <Footer/>
+    </div>
     );
-  }
-  
-  export default Brigades
+}
+
+export default Brigades
