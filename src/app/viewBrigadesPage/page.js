cd823eed8ca8e7d073428561d6e2057dbdd7463b
Felipe Flores <felipe.flores02@sap.com>
2025-05-08 21:21:16 -0300

    Add start of visualizar brigadas page

--- src/app/viewBrigadesPage/page.js
+++ src/app/viewBrigadesPage/page.js
@@ -1,23 +1,32 @@
-import Link from "next/link";
+'use client'
+
+import Button, { ButtonStyle } from "../components/button";
+import Footer from "../components/footer";
+import Header from "../components/header";
+import Input from "../components/input";
+import Icons from "../constants/icons";
 
 function ViewBrigades() {
-    return (
-      <>
-        <div>
-          Página atual: Visualizar Brigadas
+  return (
+    <>
+      <Header/>
+      <div style={{margin: "1rem"}}>
+        <div style={{display: "flex", flexWrap: "wrap"}}>
+          <span style={{color: "#39542D", fontWeight: "bolder", fontSize: "1rem", width: "100%", marginBottom: "1rem", font: "normal normal bold 24px/29px 'Montserrat'", fontFamily: "'Montserrat', sans-serif"}}>Visualizar Brigadas</span>
+
+          <span style={{color: "#39542D", width: "100%", marginBottom: "1rem", font: "normal normal normal 16px/20px 'Montserrat'", fontFamily: "'Montserrat', sans-serif"}}>Navegue pela lista ou pelo mapa para encontrar a Brigada mais próxima de você.</span>
+          <div style={{width: "100%"}}>
+            <Input placeholder={"Pesquisar Brigada"} width={"100%"}/>
+          </div>
+          <div style={{display: "flex", marginTop: "1rem", width: "100%", justifyContent: "space-between"}}>
+            <Button placeholder="Mapa"/>
+            <Button placeholder="Filtrar" style={ButtonStyle.standard} secondImage={Icons.filtrar}/>
+          </div>
         </div>
-        <div>
-          <Link href="/brigadesPage">
-              Brigades
-          </Link>
-        </div>
-        <div>
-          <Link href="/home">
-              Home
-          </Link>
-        </div>
-      </>
-    );
-  }
-  
-  export default ViewBrigades
+      </div>
+      <Footer/>
+    </>
+  );
+}
+
+export default ViewBrigades
